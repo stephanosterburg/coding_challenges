2042c1cf8bad48a8a5f982ac4bd7dfe63391fff1
Stephan Osterburg <stephanosterburg@me.com>
2019-11-23 11:14:39 -0800

    Create whatFlavor

--- hackerrank/whatFlavors.py
+++ hackerrank/whatFlavors.py
@@ -1,3 +1,19 @@
+import os
+
+
+# Sample Input
+# 2
+# 4
+# 5
+# 1 4 5 3 2
+# 4
+# 4
+# 2 2 4 3
+#
+# Sample Output
+# 1 4
+# 1 2
+#
 # They pool together money = $4; there are five flavors avaiilable.
 # The flavors 1 and 4 have a total cost of 1 + 3 = 4
 #
@@ -5,22 +21,23 @@
 # distinct flavors they choose to purchase in ascending order.
 #
 def whatFlavors(cost, money):
-    f = []
-    b = 0
-    for k, v in enumerate(reversed(cost)):
-        if b <= money:
-            if v + b <= money:
-                f.append(k + 1)
-                b += v
-            print(b)
-
-    print(' '.join(str(n) for n in f))
+    cost_dict = {}
+    for k, v in enumerate(cost):
+        if money - v in cost_dict:
+            print(str(cost_dict[money - v] + 1) + ' ' + str(k + 1))
+            return
+        else:
+            cost_dict[v] = k
 
 
-# money = 4
-# cost = [1, 4, 5, 3, 2]
-# whatFlavors(cost, money) # 1 4
+if __name__ == '__main__':
+    pwd = os.getcwd()
+    filename = open(pwd + "/ctci-ice-cream-parlor-testcases/input/input14.txt", 'r')
 
-money = 5
-cost = [1, 2, 3, 4, 5]
-whatFlavors(cost, money) # 2 3
+    t = list(map(int, filename.readline().split()))[0]
+    for t_itr in range(t):
+        money = int(filename.readline().split()[0])
+        n = filename.readline().split()[0]
+        cost = list(map(int, filename.readline().rstrip().split()))
+
+        whatFlavors(cost, money)
